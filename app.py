a628e70f028843a01c037dc742ad802b9515652e
Leonora <hi+git@leonora.app>
2025-09-17 18:35:59 +0000

    Try fix bandit issue

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,5 +9,4 @@ def home():
     return "<h1>Welcome to the DevSecOps Secure App</h1>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
-    
+    app.run(host='0.0.0.0', port=8000, debug=os.environ['HOME'] == "a")
