c93523574052ebcc09aae851d240de75127e615a
Leonora <hi+git@leonora.app>
2025-09-17 18:39:34 +0000

    Does it detect secret-looking string?

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+secret = "/j'Asv]4(bky*u2o[E|o]7pji=pQ$/I+v!Di?%$)lK;*464llLoQ}&FO\]8YVyU&"
+
 @app.route('/')
 def home():
-    return "<h1>Welcome to the DevSecOps Secure App</h1>"
+    return "<h1>Welcome to the DevSecOps Secure App</h1>"+secret
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=os.environ['HOME'] == "a")
